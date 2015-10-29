23d790bd7c859a1fead891bb5e7e387ce9e50683
rodtoll <rod.toll@gmail.com>
2015-10-28 21:20:00 -0700

    Updated test program to new usage (missed in previous checkins) and rolled version.

--- test.js
+++ test.js
@@ -1,42 +1,41 @@
 var ISY = require('./isy');
 var ISYDevice = require('./isydevice');
-var isyConstants = require('./isyconstants.js');
 
 function handleInitialized() {
 	var deviceList = isy.getDeviceList();
 	if(deviceList == null) {
-		console.log("No device list returned!");
+		ISY.debugLog("No device list returned!");
 	} else {
-		console.log("Got device list. Device count: "+deviceList.length);
+		ISY.debugLog("Got device list. Device count: "+deviceList.length);
 		for(var index = 0; index < deviceList.length; index++ ) {
-			console.log("Device: "+deviceList[index].name+", "+deviceList[index].deviceType+", "+deviceList[index].address+", "+deviceList[index].deviceFriendlyName);
+			ISY.debugLog("Device: "+deviceList[index].name+", "+deviceList[index].deviceType+", "+deviceList[index].address+", "+deviceList[index].deviceFriendlyName);
 		}
 	}
 }
 
 function handleChanged(isy, device) {
 	var logMessage = 'From isy: '+isy.address+' device changed: '+device.name;
-	if(device.deviceType == isyConstants.DEVICE_TYPE_FAN) {
+	if(device.deviceType == isy.DEVICE_TYPE_FAN) {
 		logMessage += ' fan state: '+device.getCurrentFanState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_LIGHT) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_LIGHT) {
 		logMessage += ' light state: '+device.getCurrentLightState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_DIMMABLE_LIGHT) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_DIMMABLE_LIGHT) {
 		logMessage += ' dimmable light state: '+device.getCurrentLightState()+' dimm Level: '+device.getCurrentLightDimState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_LOCK || device.deviceType == isyConstants.DEVICE_TYPE_SECURE_LOCK) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_LOCK || device.deviceType == isy.DEVICE_TYPE_SECURE_LOCK) {
 		logMessage += ' lock state: '+device.getCurrentLockState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_OUTLET) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_OUTLET) {
 		logMessage += ' outlet state: '+device.getCurrentOutletState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_ALARM_DOOR_WINDOW_SENSOR) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_ALARM_DOOR_WINDOW_SENSOR) {
 		logMessage += ' door window sensor state: '+device.getCurrentDoorWindowState()+' logical: '+device.getLogicalState()+' physical: '+device.getPhysicalState();		
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_DOOR_WINDOW_SENSOR) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_DOOR_WINDOW_SENSOR) {
 		logMessage += ' door window sensor state: '+device.getCurrentDoorWindowState();
-	} else if(device.deviceType == isyConstants.DEVICE_TYPE_ALARM_PANEL) {
+	} else if(device.deviceType == isy.DEVICE_TYPE_ALARM_PANEL) {
 		logMessage += ' alarm panel state: '+device.getAlarmStatusAsText();
 	} else {
 		logMessage += ' unknown device, cannot parse state';
 	}
 	
-    console.log(logMessage);
+    ISY.debugLog(logMessage);
 }
 
 var isy = new ISY.ISY('10.0.1.19', 'admin', 'ErgoFlat91', true, handleChanged);
